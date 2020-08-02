836e73a4eaf0ac70edbf7fe63f0e62ee7eec26d6
jethoo <jeewanbhusal@gmail.com>
2020-08-01 20:19:16 -0400

    Nav updated

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Nav({user}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link to="/" className="navbar-brand">SCMBlog</Link>
+                <Link to="/" className="navbar-brand">Jeewan's Blog</Link>
             
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
